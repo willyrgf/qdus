519744fe5e8bf3854b34bcf5f066ce9581efe3f0
Willy Romao <willyr.goncalves@gmail.com>
2014-02-07 11:52:39 -0200

    Atualizando arquivos e efetuando modificacoes.

--- qdus.sh
+++ qdus.sh
@@ -21,3 +21,11 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+DIR_FUNC="functions"
+
+source $DIR_FUNC/verification.sh
+
+_verifyUser
+
+_verifyOS
+
